1fdc65d1d4f5d634fb6a9b856e3537b79e6d634d
Ivo Nussbaumer <ivo.nussbaumer@fhnw.ch>
2020-10-23 16:27:58 +0200

    use index_col

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import os
 
 def get_preprocessed_data(path: str) -> pd.DataFrame:
     return pd.read_csv(
-        os.path.join(os.path.dirname(__file__), path)
+        os.path.join(os.path.dirname(__file__), path), index_col=0
     )
 
 def get_preprocessed_train_data() -> pd.DataFrame:
